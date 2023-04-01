75df85562fdba001a4c9e23c7c163da846cf02f1
Miikka Seppi <miikka.seppi123@gmail.com>
2023-04-02 01:23:30 +0300

    Added new modules

--- app.py
+++ app.py
@@ -5,19 +5,3 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 load_dotenv()
 app.secret_key=getenv("SECRET_KEY")
-
-@app.route("/")
-def index():
-	return render_template("index.html")
-
-@app.route("/login", methods=["POST"])
-def login():
-	username=request.form["username"]
-	password=request.form["password"]
-	session["username"]=username
-	return redirect("/")
-
-@app.route("/logout")
-def logout():
-	del session["username"]
-	return redirect("/")
---
+++ routes.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import Flask, redirect, render_template, request, session
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+	username=request.form["username"]
+	password=request.form["password"]
+	session["username"]=username
+	return redirect("/")
+
+@app.route("/logout")
+def logout():
+	del session["username"]
+	return redirect("/")
