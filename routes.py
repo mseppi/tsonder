9923cdd6237379293625979412942244ce2c2613
Miikka Seppi <miikka.seppi123@gmail.com>
2023-04-02 19:07:43 +0300

    Added requirements, fixed many bugs

--- routes.py
+++ routes.py
@@ -1,18 +1,38 @@
 from app import app
 from flask import Flask, redirect, render_template, request, session
+from userdata import login, register
+from os import urandom
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
-@app.route("/login", methods=["POST"])
-def login():
-	username=request.form["username"]
-	password=request.form["password"]
-	session["username"]=username
-	return redirect("/")
+@app.route("/login", methods=["GET", "POST"])
+def login_app():
+    if request.method=="POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if login(username, password):
+            session["user_name"] = username
+            session["csrf_token"] = urandom(16).hex()
+            return redirect('/')
+        else:
+            return "Väärä käyttäjätunnus tai salasana"
+    return render_template("login.html")
+	    
 
 @app.route("/logout")
 def logout():
 	del session["username"]
 	return redirect("/")
+
+@app.route("/register", methods=["POST"])
+def register_app():
+    username = request.form["username"]
+    password = request.form["password"]
+    role = request.form.get('role')
+    if username and password and role:
+        result=register(username, password, role)
+        if result:
+            return "Käyttäjä rekisteröity"
+    return "Rekisteröinti epäonnistui"
--- userdata.py
+++ userdata.py
@@ -1,7 +1,30 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
+from flask import session
+from os import urandom
 
-def register(name, password):
-    hash_value=generate_password_hash(password)
-    sql="INSERT INTO users (name, password) VALUES (:username, :password)"
-    db.session.execute(sql, {"name":name})
+
+def register(name, password, role):
+    try:   
+        hash_value=generate_password_hash(password)
+        sql="INSERT INTO users (name, password, role) VALUES (:username, :password, :role)"
+        db.session.execute(sql, {"name":name, "password":hash_value, "role":role})
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return False
+    return login(name, password)
+
+def login(name, password):
+    sql = "SELECT password, id, role FROM users WHERE name=:name"
+    outcome = db.session.execute(sql, {"name":name})
+    user = outcome.fetchone()
+    if check_password_hash(user[0], password):
+        if not user:
+            return False
+        session["user_name"]=name
+        session["user_id"]=user[1]
+        session["user_role"]=user[2]
+        session["csrf_token"]=urandom(16).hex()
+        return True
+    return False
